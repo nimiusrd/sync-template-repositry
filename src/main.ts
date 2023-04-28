0f9c54987d04d980e318c2f642e72a1bbcec60af
Yudai Udagawa <nimius.rd@gmail.com>
2023-04-28 13:55:41 +0000

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,22 @@
 import * as core from '@actions/core'
-import {syncTemplate} from './syncTemplate'
-import { createPullRequest } from './createPullRequest'
+import {syncTemplate} from './sync-template'
+import {createPullRequest} from './create-pull-request'
 
 async function run(): Promise<void> {
   try {
     const targetRepository: string = core.getInput('target_repository')
     const targetBranch: string = core.getInput('target_branch') ?? 'main'
-    const branchName: string = core.getInput('branch_name') ?? 'sync-template-repository'
+    const branchName: string =
+      core.getInput('branch_name') ?? 'sync-template-repository'
     const patterns: string[] = core.getInput('patterns').split(',') ?? ['**/*']
 
-    await syncTemplate(
-      {
-        patterns,
-        branchName,
-        targetRepository,
-        targetBranch
-      }
-    )
-    await createPullRequest(
-      branchName
-    )
+    await syncTemplate({
+      patterns,
+      branchName,
+      targetRepository,
+      targetBranch
+    })
+    await createPullRequest(branchName)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
